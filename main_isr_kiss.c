72a96ba7622384a4fc21f3f1bc177725181cd115
Anderson Julião <antonioaaj@al.insper.edu.br>
2025-02-18 21:52:28 -0300

    try 4

--- main_isr_kiss.c
+++ main_isr_kiss.c
@@ -1,7 +1,7 @@
 #include "asf.h"
 
-int g_cnt = 0;
-char g_str[10];
+volatile int g_cnt = 0;
+volatile char g_str[10];
 
 volatile btn_flag = 0;
 
@@ -25,7 +25,7 @@ void main(void) {
       for (i = 0; i < g_cnt; i++) {
         g_str = 1;
         g_str[i] = '*';
-        g_str[i + 1] = NULLL;
+        g_str[i + 1] = NULL;
         delay_ms(50);
         gfx_mono_draw_string(g_str, 0, 0, &sysfont);
       }
